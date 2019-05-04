f09da640cbad8dedd84b8df4fd7cadf33edb4289
ysera34 <ycyoon34@gmail.com>
2019-05-05 00:35:39 +0900

    code refactoring

--- RTT/app/src/main/java/org/inframincer/realtimetube/MainActivity.kt
+++ RTT/app/src/main/java/org/inframincer/realtimetube/MainActivity.kt
@@ -1,41 +1,32 @@
 package org.inframincer.realtimetube
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 import org.inframincer.realtimetube.dummy.DummyContent
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), VideoRecyclerViewFragment.OnListFragmentInteractionListener {
 
-    lateinit var toolbar: Toolbar
-    lateinit var tabLayout: TabLayout
-    lateinit var viewPager: ViewPager
+    private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         val adapter = VideoViewPagerAdapter(supportFragmentManager)
-        listOf("title1", "title2", "title3", "title4", "title5", "title1", "title2", "title3", "title4", "title5")
-            .forEachIndexed { index, string ->
-            adapter.addFragment(VideoRecyclerViewFragment.newInstance(index), string)
+        listOf("title1", "title2", "title3", "title4", "title5", "title6", "title7", "title8", "title9", "title10")
+            .forEachIndexed { index, title ->
+            adapter.addFragment(VideoRecyclerViewFragment.newInstance(index), title)
         }
-
-        viewPager = findViewById(R.id.viewPager)
         viewPager.adapter = adapter
-
-        tabLayout = findViewById(R.id.tabLayout)
         tabLayout.setupWithViewPager(viewPager)
     }
 
     override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
-        Log.i("MainActivity", item.toString())
+        Log.i(TAG, item.toString())
+        toast(item.toString())
     }
 }
--- RTT/app/src/main/java/org/inframincer/realtimetube/VideoRecyclerViewFragment.kt
+++ RTT/app/src/main/java/org/inframincer/realtimetube/VideoRecyclerViewFragment.kt
@@ -3,25 +3,17 @@ package org.inframincer.realtimetube
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import org.inframincer.realtimetube.dummy.DummyContent
 import org.inframincer.realtimetube.dummy.DummyContent.DummyItem
 
-/**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [VideoRecyclerViewFragment.OnListFragmentInteractionListener] interface.
- */
 class VideoRecyclerViewFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
+    private var index = 0
 
     private var listener: OnListFragmentInteractionListener? = null
 
@@ -29,7 +21,7 @@ class VideoRecyclerViewFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
+            index = it.getInt(ARG_INDEX)
         }
     }
 
@@ -38,15 +30,10 @@ class VideoRecyclerViewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_video_list, container, false)
-
-        // Set the adapter
         if (view is RecyclerView) {
             with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = VideoRecyclerViewAdapter(DummyContent.ITEMS, listener)
+                layoutManager = LinearLayoutManager(context)
+                adapter = VideoRecyclerViewAdapter(DummyContent.items, listener)
             }
         }
         return view
@@ -57,7 +44,7 @@ class VideoRecyclerViewFragment : Fragment() {
         if (context is OnListFragmentInteractionListener) {
             listener = context
         } else {
-            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+            throw RuntimeException("$context must implement OnListFragmentInteractionListener")
         }
     }
 
@@ -66,33 +53,19 @@ class VideoRecyclerViewFragment : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
     interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
         fun onListFragmentInteraction(item: DummyItem?)
     }
 
     companion object {
 
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
+        private const val ARG_INDEX = "fragment-index"
 
-        // TODO: Customize parameter initialization
         @JvmStatic
-        fun newInstance(columnCount: Int) =
+        fun newInstance(index: Int) =
             VideoRecyclerViewFragment().apply {
                 arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
+                    putInt(ARG_INDEX, index)
                 }
             }
     }
