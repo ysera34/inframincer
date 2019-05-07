300d2e86dda939c5265e4f3ce671c6bea340641b
ysera34 <ycyoon34@gmail.com>
2019-05-07 23:41:46 +0900

    junit 5, assertj and search parser

---
+++ RTT/app/src/test/java/org/inframincer/realtimetube/SearchTest.kt
@@ -0,0 +1,14 @@
+package org.inframincer.realtimetube
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class SearchTest {
+
+    @Test
+    fun `1 + 1 + 1 = 3`() {
+        val result  = 1 + 1 + 1
+        assertThat(result).isEqualTo(3)
+    }
+}
